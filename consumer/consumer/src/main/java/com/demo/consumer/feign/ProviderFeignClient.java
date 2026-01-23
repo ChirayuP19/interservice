b9ca2f1be0dd05029356d2c032fba2093aef897b
ChirayuP19 <chirayu010675@gmail.com>
2026-01-22 23:45:00 -0500

    loadBalance update into feign client

--- consumer/consumer/src/main/java/com/demo/consumer/feign/ProviderFeignClient.java
+++ consumer/consumer/src/main/java/com/demo/consumer/feign/ProviderFeignClient.java
@@ -3,7 +3,8 @@ package com.demo.consumer.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "Provider-service",url = "http://localhost:4041" )
+//@FeignClient(name = "Provider-service",url = "http://localhost:4041" )
+@FeignClient(name = "provider")
 public interface ProviderFeignClient {
 
         @GetMapping("/instance-info")
